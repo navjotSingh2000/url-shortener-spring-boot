7c1732a787ab819a3d84e9251cbb70a5675d948a
Navjot Singh <navjotsingh@Navjots-MacBook-Air.local>
2022-11-13 18:00:34 -0500

    cross-origin fix 2

--- src/main/java/com/urlshortener/shortenurl/urlshortenerservice/UrlShortener/WebController.java
+++ src/main/java/com/urlshortener/shortenurl/urlshortenerservice/UrlShortener/WebController.java
@@ -1,9 +1,11 @@
 package com.urlshortener.shortenurl.urlshortenerservice.UrlShortener;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Component
+@CrossOrigin
 public class WebController {
     @RequestMapping(value = "/index")
     public String index()
